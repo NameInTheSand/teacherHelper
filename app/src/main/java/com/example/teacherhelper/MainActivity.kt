109751d41aeeefe2fef5cb5b5ae6b57b29b79830
Sam <semernnaduev@gmail.com>
2021-04-07 13:10:53 +0300

    disable a night mode

--- app/src/main/java/com/example/teacherhelper/MainActivity.kt
+++ app/src/main/java/com/example/teacherhelper/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.teacherhelper
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.lifecycle.LiveData
 import androidx.navigation.NavController
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -14,6 +15,7 @@ class MainActivity : AppCompatActivity() {
     private var currentNavController: LiveData<NavController>? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
